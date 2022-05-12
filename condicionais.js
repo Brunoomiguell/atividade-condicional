21a2fbe68cf65d7ac46a899d0551cc4a741564d5
Brunoomiguell <103513090+Brunoomiguell@users.noreply.github.com>
2022-05-12 09:38:07 -0300

    Add files via upload

---
+++ condicionais.js
@@ -0,0 +1,40 @@
+const a = 5;
+if (a < 4) {
+    console.log('Seu valor é menor que 4')
+} else {
+     console.log('Seu valor é maior que 4')   
+}
+
+//se == if  
+
+const carroIdade = 20;
+if (carroIdade > 5){
+    console.log('carro novo')
+} else{
+    console.log('carro velho')
+}
+
+//   
+
+const nota = 4;
+// se a nota for maior ou iqual a 7 aprovada, senão , reprovada
+
+if(nota >= 7){
+    console.log('aprovado')
+}else{
+    console.log('reprovado')
+}
+
+//se o nome do Brunão for iqual a miguel, 'seu nome e bonito'senão, 'seu nome e feio'
+
+const nome = 'Brunão';
+if(nome == 'Brunão'){
+    console.log('seu nome é bonito')
+}else {
+    console.log('seu nome é feio')
+}
+
+//média
+
+alert ('ola mundo')
+prompt('digite seu valor')
---
+++ condicional-2.js
@@ -0,0 +1,37 @@
+//let n1 = prompt ('qual foi sua velocidade ')
+///let n2 = prompt ('95 klm/h')
+///let n3 = prompt ('voce foi multado ')
+
+
+//if(media >= 80){
+//    alert('voce esta certo')
+//}else{
+   // alert('voce foi multado')
+
+
+//let velocidade = prompt ('ateção ! digite aqui a sua velocidade em km/h')
+//if(velocidade > 80){
+  //  let multa = (`A sua velocidade esta acima de 80km/h. a sua multa e de  r$ ${multa}` )
+//} else{
+  //  alert('muito bem ! voce esta dentro do limite de velocidade. ')
+//}
+
+//let numero = prompt( 'Adivinhe um numero escolha entre 0 e 5')
+
+//let n1 = prompt('digite um valor')
+//if (n1 % 2 == 0){
+   // alert('Par')
+//}else{
+    //alert('Impar')
+
+//}
+
+let salario = parseInt(prompt('Digite o seu valor '))
+if(salario > 1250){
+    let aumento = (salario * 10/100)+ salario;
+    alert(aumento)
+}else {
+    let aumento2 =(salrio15/100)+salario;
+    alert(aumento2)
+}
+
